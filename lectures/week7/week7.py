ba2f6760d10d16c2eb04b92f27ac4739a6715bff
MaT1g3R <982341777@qq.com>
2016-10-26 14:23:13 -0400

    week7 lec

---
+++ lectures/week7/week7.py
@@ -0,0 +1,51 @@
+"""
+Recursion
+"""
+
+def sum_list(lst):
+    """
+    @type lst: list[int]
+    @rtype: int
+    """
+    s = 0
+    for i in lst:
+        s += i
+    return s
+
+
+def sum_list_2(lst):
+    """
+    @type lst: list[list[int]]
+    @rtype: int
+    """
+    s = 0
+    for i in lst:
+        s += sum_list(i)
+    return s
+
+
+# etc...
+
+def sum_of_nested_list(lst):
+    """
+    @type lst: list
+    @rtype: int
+    >>> sum_of_nested_list([1,2,3])
+    6
+    >>> sum_of_nested_list([1,2,[1,2]])
+    6
+    >>> sum_of_nested_list([1,2,3,4,[2,2,[1,2,[[1]]]]])
+    18
+    >>> sum_of_nested_list(5)
+    5
+    """
+    s = 0
+    if isinstance(lst, int):
+        return lst
+
+    for i in lst:
+        if isinstance(i, int):
+            s += i
+        else:
+            s += sum_of_nested_list(i)
+    return s
