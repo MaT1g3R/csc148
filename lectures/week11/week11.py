bef8379eee08353c55b93989773fe0cd8ccecabc
MaT1g3R <982341777@qq.com>
2016-11-25 14:51:15 -0500

    week11

--- lectures/week11/week11.py
+++ lectures/week11/week11.py
@@ -52,4 +52,51 @@ def merge(lst1, lst2):
 
 
 def quicksort(lst):
-    pass
+    """Return a sorted list with the same elements as <lst>.
+
+    This is a *non-mutating* version of quicksort; it does not mutate the
+    input list.
+
+    @type lst: list
+    @rtype: list
+    """
+    if len(lst) < 2:
+        return lst[:]
+    else:
+        # Pick pivot to be first element.
+        # Could make lots of other choices here (e.g., last, random)
+        pivot = lst[0]
+
+        # Partition rest of list into two halves
+        smaller, bigger = partition(lst[1:], pivot)
+
+        # Recurse on each partition
+        smaller_sorted = quicksort(smaller)
+        bigger_sorted = quicksort(bigger)
+
+        # Return! Notice the simple combining step
+        return smaller_sorted + [pivot] + bigger_sorted
+
+
+def partition(lst, pivot):
+    """Return a partition of <lst> according to the given pivot.
+
+    Return two lists, where the first is the items in <lst> that are <= pivot,
+    and the second is the items in <lst> that are > pivot.
+
+    @type lst: list
+    @type pivot: object
+    @rtype: (list, list)
+    """
+    # Use a loop to go through <lst> to build up the two
+    # lists. Note that <smaller> and <bigger> do *not* need to be sorted here.
+    smaller = []
+    bigger = []
+
+    for i in lst:
+        if i <= pivot:
+            smaller.append(i)
+        else:
+            bigger.append(i)
+
+    return smaller, bigger
